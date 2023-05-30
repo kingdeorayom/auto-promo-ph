606002c7b05b67784935f67ff2c7ff62924df791
Serking de Orayom <kingdeorayom@gmail.com>
2023-05-30 21:36:24 +0800

    update: ui fixes

--- components/Home/BudgetSearch.js
+++ components/Home/BudgetSearch.js
@@ -4,13 +4,32 @@ import Link from 'next/link'
 const BudgetSearch = () => {
 
     const boxStyle = {
-        backgroundColor: '#f5f5f5',
-        borderRadius: 5,
+        // backgroundColor: '#f5f5f5',
+        // borderRadius: 5,
+        // padding: 5,
+        // textAlign: 'center',
+        // boxShadow: '1px 1px 1px #d3d3d3',
+        // '&:hover': {
+        //     boxShadow: '1px 2px 1px #d3d3d3',
+        // }
+
+        border: '1px solid #d3d3d3',
+        borderRadius: 2,
         padding: 5,
         textAlign: 'center',
-        boxShadow: '1px 1px 1px #d3d3d3',
+        backgroundColor: '#f5f5f5',
+        // paddingX: 3,
+        // paddingY: 2.5,
+        // display: 'flex',
+        // justifyContent: 'flex-start',
+        // alignItems: 'center',
+        // height: 250,
+        // overflow: 'hidden'
         '&:hover': {
-            boxShadow: '1px 2px 1px #d3d3d3',
+            backgroundColor: '#fff',
+            border: '1px solid #1976d2',
+            // boxShadow: '1px 1px 1px #808080'
+            // color: 'primary.main'
         }
     }
 
@@ -30,7 +49,7 @@ const BudgetSearch = () => {
                 <Grid item xs={12} sm={6} lg={4}>
                     <Link href='/'>
                         <Box sx={boxStyle}>
-                            <Typography fontSize='1.2rem' fontWeight='400' color='black'>Under PHP 750,000.00</Typography>
+                            <Typography fontSize='1.2rem' fontWeight='300' color='primary'>Under PHP 750,000.00</Typography>
                         </Box>
                     </Link>
                 </Grid>
@@ -38,7 +57,7 @@ const BudgetSearch = () => {
                 <Grid item xs={12} sm={6} lg={4}>
                     <Link href='/'>
                         <Box sx={boxStyle}>
-                            <Typography fontSize='1.2rem' fontWeight='400' color='black'>Under PHP 750,000.00</Typography>
+                            <Typography fontSize='1.2rem' fontWeight='300' color='primary'>Under PHP 750,000.00</Typography>
                         </Box>
                     </Link>
                 </Grid>
@@ -46,7 +65,7 @@ const BudgetSearch = () => {
                 <Grid item xs={12} sm={6} lg={4}>
                     <Link href='/'>
                         <Box sx={boxStyle}>
-                            <Typography fontSize='1.2rem' fontWeight='400' color='black'>Under PHP 750,000.00</Typography>
+                            <Typography fontSize='1.2rem' fontWeight='300' color='primary'>Under PHP 750,000.00</Typography>
                         </Box>
                     </Link>
                 </Grid>
@@ -54,7 +73,7 @@ const BudgetSearch = () => {
                 <Grid item xs={12} sm={6} lg={4}>
                     <Link href='/'>
                         <Box sx={boxStyle}>
-                            <Typography fontSize='1.2rem' fontWeight='400' color='black'>Under PHP 750,000.00</Typography>
+                            <Typography fontSize='1.2rem' fontWeight='300' color='primary'>Under PHP 750,000.00</Typography>
                         </Box>
                     </Link>
                 </Grid>
@@ -62,7 +81,7 @@ const BudgetSearch = () => {
                 <Grid item xs={12} sm={6} lg={4}>
                     <Link href='/'>
                         <Box sx={boxStyle}>
-                            <Typography fontSize='1.2rem' fontWeight='400' color='black'>Under PHP 750,000.00</Typography>
+                            <Typography fontSize='1.2rem' fontWeight='300' color='primary'>Under PHP 750,000.00</Typography>
                         </Box>
                     </Link>
                 </Grid>
@@ -70,7 +89,7 @@ const BudgetSearch = () => {
                 <Grid item xs={12} sm={6} lg={4}>
                     <Link href='/'>
                         <Box sx={boxStyle}>
-                            <Typography fontSize='1.2rem' fontWeight='400' color='black'>Under PHP 750,000.00</Typography>
+                            <Typography fontSize='1.2rem' fontWeight='300' color='primary'>Under PHP 750,000.00</Typography>
                         </Box>
                     </Link>
                 </Grid>
--- pages/about.js
+++ pages/about.js
@@ -8,19 +8,27 @@ import PhoneIcon from '@mui/icons-material/Phone';
 const About = () => {
     return (
         <Layout>
-            <Typography fontSize='2rem' variant="h2" fontWeight='600'>About Me</Typography>
-            <Typography fontSize='1rem' variant="subtitle1" color='secondary'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Typography>
+            {/* <Typography fontSize='2rem' variant="h2" fontWeight='600'>About Me</Typography> */}
+            {/* <Typography fontSize='1rem' variant="subtitle1" color='secondary'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Typography> */}
 
             <Grid
                 container
-                mt={2}
-                mb={4}
+                mt={1}
+                mb={3}
                 rowSpacing={3}
                 columnSpacing={2}
+                justifyContent='center'
             >
 
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} md={6} sx={{ textAlign: "center" }}>
                     <Box sx={{ marginBottom: '30px' }}>
+                        <Image
+                            src={dhang_casten}
+                            width={200}
+                            height={200}
+                            alt='Dhang Casten'
+                            style={{ borderRadius: '50%', marginBottom: 10 }}
+                        />
                         <Typography fontWeight='500' variant='h1' fontSize='3rem' color='primary' mb={1}>Dhang Casten</Typography>
                         <Typography variant='h3' fontSize='1.2rem' fontWeight='500' mb={2}>Marketing Consultant</Typography>
                         <Typography variant='h3' fontSize='1.2rem' color='secondary' fontWeight='300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolores temporibus quo perferendis sequi ratione rerum eligendi veritatis omnis, adipisci eaque accusamus, veniam, illum cupiditate voluptatibus inventore tempora expedita officiis!</Typography>
