8ecdc1e9de203024ac43369a1e23a7b91c480e1b
Serking de Orayom <kingdeorayom@gmail.com>
2023-05-30 21:48:35 +0800

    update: pushed footer to bottom of page

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,12 +1,13 @@
 import Header from '../components/Layout/Header'
 import Footer from '../components/Layout/Footer'
+import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
 
     return (
         <>
             <Header />
-            <main>
+            <main className={styles.main}>
                 {children}
             </main>
             <Footer />
