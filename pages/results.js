b904c59a5f33102e0d2af409b320519b34951883
Serking de Orayom <kingdeorayom@gmail.com>
2023-06-06 06:48:16 +0800

    update: search now working

---
+++ pages/results.js
@@ -0,0 +1,70 @@
+import Layout from '@/layouts/Layout'
+import { Box, Grid, Stack, Typography } from '@mui/material'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import mitsubishi from '../public/mitsubishi-g4.webp'
+import EastIcon from '@mui/icons-material/East';
+import styles from '../styles/Vehicles.module.css'
+
+const SearchResults = () => {
+
+    const router = useRouter()
+
+    const [results, setResults] = useState([])
+
+    const fetchSearchResults = async () => {
+        const response = await fetch(`http://192.168.1.4:3001/search/${router.query.q}`);
+        const vehicle = await response.json();
+        setResults(vehicle)
+    }
+
+    useEffect(() => {
+        fetchSearchResults()
+    })
+
+    return (
+        <Layout>
+            <Typography fontSize='2rem' variant="h1" fontWeight='600' mb={1}>{`Showing search results for '${router.query.q}'`}</Typography>
+            <Typography fontSize='1rem' variant="h3" lineHeight={1.5} color='secondary'>Easily search for vehicles using our search box below</Typography>
+            <Grid
+                container
+                mt={2}
+                mb={4}
+                rowSpacing={3}
+                columnSpacing={2}
+            >
+                {
+                    results.map(result => {
+                        return (
+                            <Grid key={result._id} item xs={12} sm={6} lg={3}>
+                                <Link href={`/brands/${result.brand_slug}/${result.vehicle_slug}`}>
+                                    <Box className={styles.imageBox}>
+                                        <Image
+                                            src={mitsubishi}
+                                            alt="Mitsubishi Mirage G4"
+                                            className={styles.vehicleImage}
+                                            priority
+                                        />
+                                    </Box>
+                                    <Typography fontWeight='500' variant='h4' fontSize='1rem' mt={1.5}>{result.name}</Typography>
+                                    <Typography color='secondary'>PHP {result.price}</Typography>
+                                    <Typography color='secondary'>DP starts @ PHP 23,829.00</Typography>
+                                    <Stack direction='row' spacing={1}>
+                                        <Typography variant="button" fontWeight='500' color='primary.main'>
+                                            VIEW MORE INFORMATION
+                                        </Typography>
+                                        <EastIcon color='primary' />
+                                    </Stack>
+                                </Link>
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
+        </Layout>
+    )
+}
+
+export default SearchResults
--- pages/search.js
+++ pages/search.js
@@ -1,24 +1,40 @@
 import Layout from '@/layouts/Layout'
-import { InputAdornment, TextField, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Button, InputAdornment, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import SearchIcon from '@mui/icons-material/Search';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Search = () => {
+
+    const router = useRouter()
+
+    const [keyword, setKeyword] = useState('');
+
     return (
         <Layout>
             <Typography fontSize='2rem' variant="h1" fontWeight='600' mb={1}>Search for vehicles</Typography>
             <Typography fontSize='1rem' variant="h3" lineHeight={1.5} color='secondary'>Easily search for vehicles using our search box below</Typography>
-
             <TextField
                 color='primary'
-                // label='Search for vehicle brand, name, model or price'
-                placeholder='Search for brand, name, model or price'
+                placeholder='Search for brand, name, model or type'
                 sx={{ width: '100%', marginY: 3 }}
                 InputProps={{
                     sx: { borderRadius: 10 },
                     startAdornment: <InputAdornment position='start'><SearchIcon /></InputAdornment>
                 }}
-
+                value={keyword}
+                onChange={e => setKeyword(e.target.value)}
+                onKeyDown={e => {
+                    if (e.key === "Enter") {
+                        router.push({
+                            pathname: "/results",
+                            query: {
+                                q: keyword
+                            }
+                        })
+                    }
+                }}
             />
 
         </Layout>
