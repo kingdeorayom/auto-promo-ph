7e0138bc5e050dfa3c0866a49ccbf97ed1fd971f
Serking de Orayom <kingdeorayom@gmail.com>
2023-06-06 08:20:30 +0800

    update: fixed search box render

--- pages/search.js
+++ pages/search.js
@@ -5,37 +5,44 @@ import SearchIcon from '@mui/icons-material/Search';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-const Search = () => {
+const SearchBox = () => {
 
     const router = useRouter()
-
     const [keyword, setKeyword] = useState('');
+
+    return (
+        <TextField
+            color='primary'
+            placeholder='Search for brand, name, model or type'
+            sx={{ width: '100%', marginY: 3 }}
+            InputProps={{
+                sx: { borderRadius: 10 },
+                startAdornment: <InputAdornment position='start'><SearchIcon /></InputAdornment>
+            }}
+            value={keyword}
+            onChange={e => setKeyword(e.target.value)}
+            onKeyDown={e => {
+                if (e.key === "Enter") {
+                    router.push({
+                        pathname: "/results",
+                        query: {
+                            q: keyword
+                        }
+                    })
+                }
+            }}
+        />
+    )
+}
+
+const Search = () => {
 
     return (
         <Layout>
             <Typography fontSize='2rem' variant="h1" fontWeight='600' mb={1}>Search for vehicles</Typography>
             <Typography fontSize='1rem' variant="h3" lineHeight={1.5} color='secondary'>Easily search for vehicles using our search box below</Typography>
-            <TextField
-                color='primary'
-                placeholder='Search for brand, name, model or type'
-                sx={{ width: '100%', marginY: 3 }}
-                InputProps={{
-                    sx: { borderRadius: 10 },
-                    startAdornment: <InputAdornment position='start'><SearchIcon /></InputAdornment>
-                }}
-                value={keyword}
-                onChange={e => setKeyword(e.target.value)}
-                onKeyDown={e => {
-                    if (e.key === "Enter") {
-                        router.push({
-                            pathname: "/results",
-                            query: {
-                                q: keyword
-                            }
-                        })
-                    }
-                }}
-            />
+
+            <SearchBox />
 
         </Layout>
     )
