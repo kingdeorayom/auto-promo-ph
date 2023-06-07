4e302dc5cd5f1640da351bc632e88d4c3f3ef385
Serking de Orayom <kingdeorayom@gmail.com>
2023-06-07 08:29:49 +0800

    update: inquiry validation

---
+++ data/validation/inquiry.js
@@ -0,0 +1,27 @@
+import * as Yup from 'yup'
+
+export const inquiry_validation = Yup.object().shape({
+    name:
+        Yup.string()
+            .required()
+            .label("Name"),
+    email:
+        Yup
+            .string()
+            .required()
+            .email()
+            .matches(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, "Please enter a valid email")
+            .label("Email"),
+    mobileNumber:
+        Yup.string()
+            .min(11)
+            .max(11)
+            .required()
+            .label("Mobile Number")
+            .matches(/^[0-9]*$/, "This field only accepts numerical values"),
+    message:
+        Yup.string()
+            .required()
+            .label("Message"),
+
+});
