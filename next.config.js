05e0df0a17398d46cac2d65158ad65d073b10137
Serking de Orayom <kingdeorayom@gmail.com>
2023-06-15 22:13:56 +0800

    update: image config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['192.168.1.2'],
-  },
   // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'auto-promo-ph-api.onrender.com',
-  //       port: '',
-  //       pathname: '/images/**',
-  //     },
-  //   ],
+  //   domains: ['192.168.1.2'],
   // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'auto-promo-ph-api.onrender.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
