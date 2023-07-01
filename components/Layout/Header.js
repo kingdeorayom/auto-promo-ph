4599ceda274a01a21eeb8e9c28f3987a64b76384
Serking de Orayom <kingdeorayom@gmail.com>
2023-07-01 23:32:13 +0800

    update: nav links redesign

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -6,30 +6,14 @@ import { AppBar, Box, IconButton, Stack, Toolbar, Typography } from "@mui/materi
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
 import logo from '@/public/logotosvg.png'
-import gmail_icon from '@/public/gmail_icon.svg'
-import viber_icon from '@/public/viber_icon.svg'
-import facebook_icon from '@/public/facebook_icon.svg'
 import styles from '@/styles/Header.module.css'
 import Drawer from "./Drawer";
-import { useAuthContext } from "@/hooks/useAuthContext";
 import nookies from 'nookies'
 import { grey } from '@mui/material/colors';
 
 const Header = () => {
 
     const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-    const selectedLink = '#FFDE00'
-    // const selectedLink = '#ffffff'
-    const unSelectedLink = '#ffffff'
-
-    // const navigation_item_override = {
-    //     px: '16px',
-    //     py: '8px',
-    //     '&:hover': {
-    //         borderBottom: `2px solid ${selectedLink}`
-    //     }
-    // }
 
     const cookies = nookies.get()
 
@@ -40,12 +24,7 @@ const Header = () => {
 
     const router = useRouter()
 
-    const { user } = useAuthContext()
-
     const [isDrawerOpen, setIsDrawerOpen] = useState(false)
-
-    const fromMediumDisplayWithoutDashboard = { xs: 'none', sm: 'none', md: 'flex' }
-    const fromMediumDisplayWithDashboard = { xs: 'none', sm: 'none', lg: 'flex' }
 
     const menuIconDisplayWithoutDashboard = { xs: 'flex', sm: 'flex', md: 'none' }
     const menuIconDisplayWithDashboard = { xs: 'flex', md: 'flex', lg: 'none' }
@@ -58,9 +37,6 @@ const Header = () => {
             <AppBar
                 position='sticky'
                 elevation={0}
-                sx={{
-                    // boxShadow: '0 0 4px 2px rgba(36, 39, 44, 0.15)',
-                }}
             >
                 <Toolbar className={styles.toolbar}>
                     <Box>
@@ -77,28 +53,28 @@ const Header = () => {
                         {isLoggedIn ?
                             <Link href='/admin/dashboard'>
                                 <Box className={router.pathname == '/admin/dashboard' ? styles.navLinkContainer : styles.navLinkContainerUnselected}>
-                                    <Typography fontSize='1rem' fontWeight={router.pathname == '/admin/dashboard' ? "700" : "400"} color={router.pathname == '/admin/dashboard' ? selectedLink : unSelectedLink}>Dashboard</Typography>
+                                    <Typography fontSize='1rem' fontWeight='700'>Dashboard</Typography>
                                 </Box>
                             </Link> : null
                         }
                         <Link href='/'>
                             <Box className={router.pathname == '/' ? styles.navLinkContainer : styles.navLinkContainerUnselected}>
-                                <Typography fontSize='1rem' fontWeight={router.pathname == '/' ? "700" : "400"} color={router.pathname == '/' ? selectedLink : unSelectedLink}>Explore</Typography>
+                                <Typography fontSize='1rem' fontWeight='700'>Explore</Typography>
                             </Box>
                         </Link>
                         <Link href='/brands'>
                             <Box className={router.pathname == '/brands' ? styles.navLinkContainer : styles.navLinkContainerUnselected}>
-                                <Typography fontSize='1rem' fontWeight={router.pathname == '/brands' ? "700" : "400"} color={router.pathname == '/brands' ? selectedLink : unSelectedLink}>Brands</Typography>
+                                <Typography fontSize='1rem' fontWeight='700'>Brands</Typography>
                             </Box>
                         </Link>
                         <Link href='/promos'>
                             <Box className={router.pathname == '/promos' ? styles.navLinkContainer : styles.navLinkContainerUnselected}>
-                                <Typography fontSize='1rem' fontWeight={router.pathname == '/promos' ? "700" : "400"} color={router.pathname == '/promos' ? selectedLink : unSelectedLink}>Promos</Typography>
+                                <Typography fontSize='1rem' fontWeight='700'>Promos</Typography>
                             </Box>
                         </Link>
                         <Link href='/contact'>
                             <Box className={router.pathname == '/contact' ? styles.navLinkContainer : styles.navLinkContainerUnselected}>
-                                <Typography fontSize='1rem' fontWeight={router.pathname == '/contact' ? "700" : "400"} color={router.pathname == '/contact' ? selectedLink : unSelectedLink}>Contact</Typography>
+                                <Typography fontSize='1rem' fontWeight='700'>Contact</Typography>
                             </Box>
                         </Link>
                     </Stack>
@@ -122,8 +98,6 @@ const Header = () => {
                     </Stack>
                 </Toolbar>
             </AppBar>
-
-            {/* <Navbar router={router} fromMediumDisplay={fromMediumDisplay} /> */}
 
             <Drawer
                 isDrawerOpen={isDrawerOpen}
